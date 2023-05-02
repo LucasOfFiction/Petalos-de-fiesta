00bf6c523d8854089817b569816e3955f4287650
Ricky <rickyth12@gmail.com>
2023-05-03 02:00:01 +0300

    Inclusion de footer en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from 'react';
+import React from "react";
+import ReactDOM from "react-dom";
+import Footer from "./components/footer"
+
 
 function App(){
     return(
-        <h1>Pétalos de fiesta</h1>
-    )
 
-}
+        <div>
+            <h1>Pétalos de fiesta</h1>
+            <Footer/>
+
+        </div>
+        
+    )}
 
 
 export default App;
