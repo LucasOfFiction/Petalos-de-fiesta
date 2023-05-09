522cabd62db1287ad3328ff09510180ae029b8dc
borrellmanuel1@gmail.com <borrellmanuel1@gmail.com>
2023-05-09 19:44:17 -0300

    solocuino error de merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
 import Aboutus from './components/AboutUs.jsx';
 import Dedndlasflores from './components/Dondelasflores.jsx';
+import { useState } from 'react';
+import React from "react";
+import ReactDOM from "react-dom";
+import Footer from "./components/footer"
+import ProductList from './components/productList';
+import Slider from './components/Slider';
 
 
 function App(){
+    const mockImagenes = [ //imagenes de prueba 
+		'https://picsum.photos/id/1020/400',
+		'https://picsum.photos/id/1025/400',
+		'https://picsum.photos/id/1010/400',
+        'https://picsum.photos/id/1011/400',
+        'https://picsum.photos/id/1012/400',
+	];
     return (
         
         <div>
+             <h1>Pétalos de fiesta</h1>
+            <Slider imagenes={mockImagenes} />;
             <Aboutus />
             <Dedndlasflores />
+            <Footer/>
         </div>
 
     ); 
