fc3780d847ed2b1695267d10f81c355a1a6a1c07
JSDRAKE <jsdrake@jsdrake.com.ar>
2023-05-10 13:09:57 -0300

    Se agrego el precio anterior a la oferta

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -2,14 +2,14 @@ import '../styles/product.css';
 
 const Product = ({data, addToCart}) => {
     
-    const {id, name, price, img} = data;
+    const {id, name, price, old, img} = data;
 
     return (
 
         <div className='product_box' >
             <img className='product_img' src={img} alt={name} width="250px" height='200px' />
             <h3> {name} </h3>
-            <h4 className='product_price' > ${price} </h4>
+            <h4 className='product_price' > ${price} <span className='old_price' > ${old} </span> </h4>
             <button className='product_btn' onClick={() => addToCart(id)}>Agregar al Carro</button>
         </div>
     );
