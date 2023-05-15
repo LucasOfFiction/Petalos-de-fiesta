f13a37f056822c4f2fe0f541ba240410942f17d0
Ricky <rickyth12@gmail.com>
2023-05-15 23:53:28 +0300

    Cambie de lugar Carpeta footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import REDES from "./footerComponents/redes";
-import "../components/footer.css";
+import "../styles/footer.css";
 import LINKS from "./footerComponents/links";
 
 
