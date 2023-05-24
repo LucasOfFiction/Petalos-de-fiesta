92d21d606ff57226c10f635fe56a6694b6ab104c
LucasOfFiction <lucas.2003.566@gmail.com>
2023-05-24 16:32:44 -0400

    se arregla responsive

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -56,6 +56,9 @@ export default function NavBar(){
                                 <img src={logo} alt="" width='100px'/>
                             </a>
 
+
+
+
                             {/* MENÚ HAMBURGUESA */}
                             <button className="navbar-toggler" 
                             type="button"
@@ -69,6 +72,12 @@ export default function NavBar(){
                             </button>
                             {/* /MENU HAMBURGUESA */}
                             
+
+
+
+
+
+
 
                             <div className="collapse navbar-collapse" id="navbarSupportedContent">
 
@@ -108,6 +117,9 @@ export default function NavBar(){
                                     <hr className='text-dark-50'></hr>
 
                             {/*ICONOS DEL LADO DERECHO DE LA PÁGINA*/}
+
+
+                                {/*
                                     <ul id='ul2' className="navbar-nav flex-row flex-wrap  ms-auto">
                                         <li id='li' className='nav-item col-4 col-sm-auto p-2'>
                                             <button id='redondo' className="redondo">
@@ -123,7 +135,6 @@ export default function NavBar(){
                                             </button>
                                         </li>
 
-                            {/*ICONO DEL CARRITO*/}
                                         <li id='li' className='nav-item col-4 col-sm-auto p-2'>
                                                 <button id='redondo' className="">
 
@@ -141,9 +152,49 @@ export default function NavBar(){
 
                                                 </button>
                                         </li>
-                            {/*/ICONO DEL CARRITO*/}
                                     </ul>
+                                    */}
+
+
                             {/* /ICONOS DEL LADO DERECHO DE LA PAGINA*/}
+                                <div className="navbar-nav flex-row flex-wrap  ms-auto" >
+                                    
+                                        <li className='nav-item col-4 col-sm-auto p-2'>
+                                            <button id='redondo' className="redondo">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+                                                    <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"/>
+                                                </svg>
+                                            </button>
+                                        </li>
+
+                                        <li className='nav-item col-4 col-sm-auto p-2'>
+                                            <button id='redondo' className=""> 
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                                                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                                                </svg>
+                                            </button>
+                                        </li>
+
+                                        <li className='nav-item col-4 col-sm-auto p-2'>
+                                            <button id='redondo' className="">
+
+                                                <Badge badgeContent={CartArray.length} color="primary"
+                                                    id="basic-button"
+                                                    aria-controls={open ? 'basic-menu' : undefined}
+                                                    aria-haspopup="true"
+                                                    aria-expanded={open ? 'true' : undefined}
+                                                    onClick={handleClick}
+                                                >
+                                                    <svg xmlns="http://www.w3.org/2000/svg" id="carti" width="20" height="20" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+                                                        <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                                                    </svg> 
+                                                </Badge>
+
+                                            </button>
+                                        </li>
+                                    
+                                </div>
+                                
                             </div>
 
                     </div>
@@ -159,45 +210,14 @@ export default function NavBar(){
                     open={open}
                     onClose={handleClose}
                     MenuListProps={{
-                    'aria-labelledby': 'basic-button',
+                        'aria-labelledby': 'basic-button',
                     }}
                 >
 
                     {
                         CartArray.length ? 
                         <div className='card_details' style={{width:"24rem", padding:"10"}}>
-
-                            {/*<Table>
-                                <thead>
-                                    <tr>
-                                        <th>Producto</th>
-                                        <th>Detalles</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {
-                                        CartArray.map((e)=>{
-                                            return(
-                                                <>
-                                                <tr>
-                                                    <td>
-                                                        <img src={e.img} style={{width:"5rem", height:"5rem"}} alt="" />
-                                                    </td>
-                                                    <td>
-                                                        <p>{e.name}</p>
-                                                        <p>Precio: {e.price}$</p>
-                                                        <p>Cantidad: {e.quantity}</p>
-                                                        <p style={{color:"red", fontSize:"20", cursor:"pointer"}}>
-                                                            <DeleteIcon onClick={() => deleteFromCart(id, true)} ></DeleteIcon>
-                                                        </p>
-                                                    </td>
-                                                </tr>
-                                                </>
-                                            )
-                                        })
-                                    }
-                                </tbody>
-                            </Table>*/}
+                            
                             <i  className='fas fa-close smallclose'onClick={handleClose}
                                 style={{position:"absolute",top:2,right:20,fontSize:23,cursor:"pointer"}} >
                             </i>
